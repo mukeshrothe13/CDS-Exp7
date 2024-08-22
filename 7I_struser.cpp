91afa23170fb60dafdf68da5860741de6e0c5828
mukeshrothe13 <rothemukesh7@gmail.com>
2024-08-22 22:22:13 +0530

    7I_struser.cpp

--- 7I_struser.cpp
+++ 7I_struser.cpp
@@ -1,10 +1,9 @@
-//PRAKHAR GUPTA
-//23070123101
+//Mukesh Rothe  //23070123089  //EXP 7
 #include <iostream>
 using namespace std;
 int main()
 {
-    char s[]= "Prakhar";
+    char s[]= "Mukesh";
     cout<<s<<endl;
     return 0;
 }
